f4c7a6710697ed3a6251d5a1d0fb62ba837ec68b
Facundo <vinas335@gmail.com>
2024-10-02 21:41:45 -0300

    v7

--- js/Calendario.js
+++ js/Calendario.js
@@ -1,1 +1,224 @@
+const contenedorDias = document.querySelector(".days");
+const botonSiguiente = document.querySelector(".next");
+const botonAnterior = document.querySelector(".prev");
+const botonHoy = document.querySelector(".today");
+const mesElemento = document.querySelector(".month");
 
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const diasSemana = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+
+const fechaActual = new Date();
+let mesActual = fechaActual.getMonth();
+let añoActual = fechaActual.getFullYear();
+
+const actualizarCalendario = () => {
+  fechaActual.setDate(1);
+  const primerDia = new Date(añoActual, mesActual, 1);
+  const ultimoDia = new Date(añoActual, mesActual + 1, 0);
+  const indiceUltimoDia = ultimoDia.getDay();
+  const fechaUltimoDia = ultimoDia.getDate();
+  const ultimoDiaMesAnterior = new Date(añoActual, mesActual, 0);
+  const fechaUltimoDiaMesAnterior = ultimoDiaMesAnterior.getDate();
+  const diasSiguientes = 7 - indiceUltimoDia - 1;
+
+  mesElemento.innerHTML = `${meses[mesActual]} ${añoActual}`;
+
+  let dias = "";
+
+  for (let x = primerDia.getDay(); x > 0; x--) {
+    dias += `<div class="day prev">${fechaUltimoDiaMesAnterior - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= fechaUltimoDia; i++) {
+    if (
+      i === new Date().getDate() &&
+      mesActual === new Date().getMonth() &&
+      añoActual === new Date().getFullYear()
+    ) {
+      dias += `<div class="day today">${i}</div>`;
+    } else {
+      dias += `<div class="day">${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= diasSiguientes; j++) {
+    dias += `<div class="day next">${j}</div>`;
+  }
+
+  contenedorDias.innerHTML = dias;
+  ocultarBotonHoy();
+};
+
+botonSiguiente.addEventListener("click", () => {
+  mesActual++;
+  if (mesActual > 11) {
+    mesActual = 0;
+    añoActual++;
+  }
+  actualizarCalendario();
+});
+
+botonAnterior.addEventListener("click", () => {
+  mesActual--;
+  if (mesActual < 0) {
+    mesActual = 11;
+    añoActual--;
+  }
+  actualizarCalendario();
+});
+
+botonHoy.addEventListener("click", () => {
+  mesActual = fechaActual.getMonth();
+  añoActual = fechaActual.getFullYear();
+  actualizarCalendario();
+});
+
+function ocultarBotonHoy() {
+  if (
+    mesActual === new Date().getMonth() &&
+    añoActual === new Date().getFullYear()
+  ) {
+    botonHoy.style.display = "none";
+  } else {
+    botonHoy.style.display = "flex";
+  }
+}
+
+actualizarCalendario();
+
+contenedorDias.addEventListener("click", (e) => {
+  if (e.target.classList.contains("day") && !e.target.classList.contains("prev") && !e.target.classList.contains("next")) {
+      const dia = e.target.innerText;
+      const fechaSeleccionada = new Date(añoActual, mesActual, dia);
+
+      Swal.fire({
+        title: `Ingrese un evento para el ${fechaSeleccionada.toLocaleDateString()}`,
+        input: 'text',
+        inputPlaceholder: 'Escriba su tarea aquí',
+        showCancelButton: true,
+        confirmButtonText: 'Agregar',
+        cancelButtonText: 'Cancelar',
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Debe ingresar una tarea válida.';
+          }
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const textoTarea = result.value;
+
+          const nuevaTarea = {
+            texto: textoTarea,
+            completado: false,
+            fecha: fechaSeleccionada.toISOString()
+          };
+
+          let diaTarea = nuevaTarea.fecha.slice(8, 10);
+          let mesTarea = nuevaTarea.fecha.slice(5, 7);
+
+          guardartareas.push(nuevaTarea.texto + ` - ${diaTarea}/${mesTarea}`);
+          localStorage.setItem("tareas", JSON.stringify(guardartareas));
+
+          let li = document.createElement("li");
+          let div = document.createElement("div");
+          div.classList.add("div_li");
+
+          li.innerHTML = `<p>${nuevaTarea.texto} - ${diaTarea}/${mesTarea}</p>`;
+
+          let botonBorrarIndividual = document.createElement("button");
+          botonBorrarIndividual.classList.add("botonBorrarIndividual");
+          
+          botonBorrarIndividual.addEventListener("click", () => {
+            li.remove();
+            eliminarTareaDeLocalStorage(nuevaTarea);
+            Toastify({
+              text: "Completado",
+              position: "left",
+              duration: 3000
+            }).showToast();
+          });
+
+          let iconoBorrar = document.createElement("i");
+          iconoBorrar.classList.add("fas", "fa-check");
+          botonBorrarIndividual.appendChild(iconoBorrar);
+
+          li.appendChild(botonBorrarIndividual);
+          div.appendChild(li);
+          listaDeTareas.appendChild(div);
+
+          if (parseInt(diaTarea) === new Date().getDate() && parseInt(mesTarea) === new Date().getMonth() + 1) {
+            Toastify({
+              text: "Es para hoy",
+              position: "left",
+              duration: 3000
+            }).showToast();
+          }
+        }
+      });
+  }
+});
+
+let texto = document.getElementById("fecha");
+
+function convertirFecha(fechaString) {
+  let partes = fechaString.split('/');
+  let dia = partes[0];
+  let mes = partes[1];
+  let año = partes[2];
+
+  return new Date(`${año}-${mes}-${dia}`);
+}
+
+function calcularDiferenciaEnDias(fecha1, fecha2) {
+  const diferenciaMs = fecha2 - fecha1;
+  return Math.floor(diferenciaMs / (1000 * 60 * 60 * 24));
+}
+
+function consultarFeriados() {
+    fetch("https://api.argentinadatos.com/v1/feriados/2024")
+      .then(response => response.json())
+      .then(data => {
+        let fechaActual = new Date();
+
+        let feriadoMasCercano = data
+          .map(feriado => {
+            return {
+              ...feriado,
+              fechaObj: convertirFecha(feriado.fecha)
+            };
+          })
+          .filter(feriado => feriado.fechaObj >= fechaActual)
+          .sort((a, b) => a.fechaObj - b.fechaObj)[0];
+
+        if (feriadoMasCercano) {
+          let dia = feriadoMasCercano.fechaObj.getDate();
+          let mes = feriadoMasCercano.fechaObj.getMonth() + 1;
+
+          let diasFaltantes = calcularDiferenciaEnDias(fechaActual, feriadoMasCercano.fechaObj );
+
+          texto.innerHTML = `${dia}/${mes} ${feriadoMasCercano.nombre} , faltan ${diasFaltantes +1} días.`;
+        } else {
+          texto.innerHTML = 'No hay feriados futuros en este año';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        texto.innerHTML = 'Error al cargar los feriados';
+      });
+}
+
+consultarFeriados();
--- js/main.js
+++ js/main.js
@@ -134,6 +134,7 @@ const calcularfecha = (fecha) => {
     return edad;
 };
 
+
 const calculardias = (fecha) => {
     const fechahoy = new Date();
     const diahoy = fechahoy.getDate();
@@ -142,11 +143,10 @@ const calculardias = (fecha) => {
 
     const diaCumple = parseInt(fecha.substring(8, 10));
     const mesCumple = parseInt(fecha.substring(5, 7));
-    
 
     if (diahoy === diaCumple && meshoy === mesCumple) {
         Swal.fire({
-            title: "Feliz cumpleaños.",
+            title: "¡Feliz cumpleaños!",
             imageUrl: "https://plus.unsplash.com/premium_vector-1682299666311-ef9c9836ae60?q=80&w=1368&auto=format&fit=crop",
             imageWidth: 400,
             imageHeight: 200,
@@ -163,193 +163,17 @@ const calculardias = (fecha) => {
 };
 
 calcular.addEventListener("click", () => {
-    resultado.innerText = `Tu edad es ${calcularfecha(fecha.value)}`;
-    calculardias(fecha.value);
-});
-
-
-
-const daysContainer = document.querySelector(".days");
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
-const todayBtn = document.querySelector(".today");
-const month = document.querySelector(".month");
-
-const months = [
-  "Enero",
-  "Febrero",
-  "Marzo",
-  "Abril",
-  "Mayo",
-  "Junio",
-  "Julio",
-  "Agosto",
-  "Septiembre",
-  "Octubre",
-  "Noviembre",
-  "Diciembre",
-];
-
-const days = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
-
-const date = new Date();
-let currentMonth = date.getMonth();
-let currentYear = date.getFullYear();
-
-const renderCalendar = () => {
-  date.setDate(1);
-  const firstDay = new Date(currentYear, currentMonth, 1);
-  const lastDay = new Date(currentYear, currentMonth + 1, 0);
-  const lastDayIndex = lastDay.getDay();
-  const lastDayDate = lastDay.getDate();
-  const prevLastDay = new Date(currentYear, currentMonth, 0);
-  const prevLastDayDate = prevLastDay.getDate();
-  const nextDays = 7 - lastDayIndex - 1;
-
-  month.innerHTML = `${months[currentMonth]} ${currentYear}`;
-
-  let days = "";
-
-  for (let x = firstDay.getDay(); x > 0; x--) {
-    days += `<div class="day prev">${prevLastDayDate - x + 1}</div>`;
-  }
-
-  for (let i = 1; i <= lastDayDate; i++) {
-    if (
-      i === new Date().getDate() &&
-      currentMonth === new Date().getMonth() &&
-      currentYear === new Date().getFullYear()
-    ) {
-      days += `<div class="day today">${i}</div>`;
+    if (!fecha.value) {
+        Toastify({
+            text: "Por favor, ingresa una fecha.",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            backgroundColor: "#f44336",
+            stopOnFocus: true,
+        }).showToast();
     } else {
-      days += `<div class="day">${i}</div>`;
+        resultado.innerText = `Tu edad es ${calcularfecha(fecha.value)}`;
+        calculardias(fecha.value);
     }
-  }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="day next">${j}</div>`;
-  }
-
-  daysContainer.innerHTML = days;
-  hideTodayBtn();
-};
-
-nextBtn.addEventListener("click", () => {
-  currentMonth++;
-  if (currentMonth > 11) {
-    currentMonth = 0;
-    currentYear++;
-  }
-  renderCalendar();
-});
-
-prevBtn.addEventListener("click", () => {
-  currentMonth--;
-  if (currentMonth < 0) {
-    currentMonth = 11;
-    currentYear--;
-  }
-  renderCalendar();
-});
-
-todayBtn.addEventListener("click", () => {
-  currentMonth = date.getMonth();
-  currentYear = date.getFullYear();
-  renderCalendar();
-});
-
-function hideTodayBtn() {
-  if (
-    currentMonth === new Date().getMonth() &&
-    currentYear === new Date().getFullYear()
-  ) {
-    todayBtn.style.display = "none";
-  } else {
-    todayBtn.style.display = "flex";
-  }
-}
-
-renderCalendar();
-
-// Evento de clic para los días del calendario
-daysContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("day") && !e.target.classList.contains("prev") && !e.target.classList.contains("next")) {
-      const day = e.target.innerText;  // Capturar el día seleccionado
-      const selectedDate = new Date(currentYear, currentMonth, day);  // Crear la fecha con el año y mes actual
-
-      // Mostrar SweetAlert para ingresar la tarea
-      Swal.fire({
-        title: `Ingrese un evento para el ${selectedDate.toLocaleDateString()}`,
-        input: 'text',
-        
-        inputPlaceholder: 'Escriba su tarea aquí',
-        showCancelButton: true,
-        confirmButtonText: 'Agregar',
-        cancelButtonText: 'Cancelar',
-        inputValidator: (value) => {
-          if (!value) {
-            return 'Debe ingresar una tarea válida.';
-          }
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          const taskText = result.value;
-
-          // Crear una nueva tarea
-          const newTask = {
-            text: taskText,  // La tarea ingresada por el usuario
-            completed: false,
-            date: selectedDate.toISOString()
-          };
-
-          let diaTarea = newTask.date.slice(8, 10);
-          let mesTarea = newTask.date.slice(5, 7);
-
-          // Agregar la nueva tarea a la lista
-          guardartareas.push(newTask.text + ` - ${diaTarea}/${mesTarea}`);
-          
-          localStorage.setItem("tareas", JSON.stringify(guardartareas));  // Guardar en localStorage
-         
-          // Mostrar la nueva tarea en la lista
-          let li = document.createElement("li");
-          let div = document.createElement("div");
-          div.classList.add("div_li");
-
-          li.innerHTML = `<p>${newTask.text} - ${diaTarea}/${mesTarea}</p>`;
-          
-
-          let botonBorrarIndividual = document.createElement("button");
-          botonBorrarIndividual.classList.add("botonBorrarIndividual");
-          
-          botonBorrarIndividual.addEventListener("click", () => {
-            li.remove();
-            eliminarTareaDeLocalStorage(newTask);  // Eliminar la tarea del localStorage
-            Toastify({
-              text: "Completado",
-              position: "left",
-              duration: 3000
-            }).showToast();
-          });
-
-          let iconoBorrar = document.createElement("i");
-          iconoBorrar.classList.add("fas", "fa-check");
-          botonBorrarIndividual.appendChild(iconoBorrar);
-
-          li.appendChild(botonBorrarIndividual);
-          div.appendChild(li);
-          listaDeTareas.appendChild(div);
-
-          // Si la tarea es para hoy, mostrar alerta
-          if (parseInt(diaTarea) === new Date().getDate() && parseInt(mesTarea) === new Date().getMonth() + 1) {
-            // Mostrar alerta si la tarea es para hoy
-            Toastify({
-              text: "Es para hoy",
-              position: "left",
-              duration: 3000
-            }).showToast();
-          }
-          
-        }
-      });
-  }
 });
